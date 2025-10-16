c5f44219e42f43e3b8ca35177edece0a113a8c84
JABNIA <vaxojabnia@gmail.com>
2025-10-16 11:10:04 +0400

    added suggested product component

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,15 +1,12 @@
-import { useTranslation } from "react-i18next";
 import Banner from "./banner/Banner"
 import ProductSlider from "./productSlider/ProductSlider"
 
 function HomePage() {
-  const { i18n } = useTranslation();
     
   return (
     <div>
       <Banner />
       <ProductSlider />
-
     </div>
   )
 }
